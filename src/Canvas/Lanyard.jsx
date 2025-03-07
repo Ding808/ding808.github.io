e9c3a5b8c6cbb9f0b72f035cd55582ad894075b6
Yueyang Ding <dingyeuyang@gmail.com>
2025-03-07 13:42:41 -0500

    🐛 image lanyard missing bug fixed

--- src/Canvas/Lanyard.jsx
+++ src/Canvas/Lanyard.jsx
@@ -6,7 +6,7 @@ import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphe
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
 
 import cardGLB from "./card.glb";
-import lanyard from "../../dist/images/lanyard.png";
+import lanyard from "../../public/images/lanyard.png";
 
 import * as THREE from 'three';
 import './Lanyard.css';
