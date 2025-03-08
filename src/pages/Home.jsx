983ac32c52a404818abdd32057a1d051b3aa79c7
Yueyang Ding <dingyeuyang@gmail.com>
2025-03-07 22:53:36 -0500

    📑 Home page background section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import ParticleBackground from '../Canvas/ParticleBackground'; 
 import BlurText from "../Canvas/BlurText";
+import './Home.css';  // 引入外部样式文件
+
 const handleAnimationComplete = () => {
   console.log('Animation completed!');
 };
 
-
-
 function Home() {
   return (
-    <>
-      <div>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-          <BlurText
+    <div style={{ position: 'relative', overflow: 'hidden' }}>
+      {/* 背景半透明灰色区 */}
+      <div className="fadeInBackground" />
+
+      {/* 内容层，z-index比背景高 */}
+      <div style={{ position: 'relative', zIndex: 2 }}>
+        <div>
+          <h1 style={{ color: 'white', textAlign: 'center' }}>
+            <BlurText
               text="Hey there !!"
               delay={150}
               animateBy="words"
@@ -19,26 +24,29 @@ function Home() {
               onAnimationComplete={handleAnimationComplete}
               className="text-2xl mb-8"
             />
-        </h1>
-      </div>
-      <div>
-        <a target="_blank" rel="noreferrer">
-          <img
-            src="/images/ProfileImage.jpg"
-            className="logo"
-            alt="My profile image"
-          />
-        </a>
-      </div>
-      <h1 style={{ color: 'white', textAlign: 'center' }}><BlurText
+          </h1>
+        </div>
+        <div>
+          <a target="_blank" rel="noreferrer">
+            <img
+              src="/images/ProfileImage.jpg"
+              className="logo"
+              alt="My profile image"
+            />
+          </a>
+        </div>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          <BlurText
             text="Welcome to my own Portfolio Website !!!"
             delay={150}
             animateBy="words"
             direction="top"
             onAnimationComplete={handleAnimationComplete}
             className="text-2xl mb-8"
-          /></h1>
-    </>
+          />
+        </h1>
+      </div>
+    </div>
   );
 }
 
